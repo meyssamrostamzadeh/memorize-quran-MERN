044c684f5bfc4e54b8ced10d3ed2831225fce65d
meyssam Rostamzadeh <meyssam.rostamzadeh@gmail.com>
2023-09-15 13:46:32 +0330

    Create MovieListItem.jsx

---
+++ frontend/src/components/MovieListItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./MovieListItem.css";
+
+function Movie({ movie, onDeleteMovie }) {
+  return (
+    <li className="MovieListItem">
+      {movie.title}
+      {movie.id}
+      {movie.sura}
+      {movie.part}
+      {movie.color}
+      <button className="MovieListItem__Delete" onClick={onDeleteMovie}>
+        <img src="/images/delete.svg" alt="Delete movie" />
+      </button>
+    </li>
+  );
+}
+
+export default Movie;
